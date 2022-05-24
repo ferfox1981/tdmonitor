d1208f06603a69524d332a32f3c3051260c5d256
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2022-05-24 20:23:58 -0300

    comment

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var Twit = require("twit");
 const taxa = require('./monitor.js')
 
-var redis = require('redis');
+
 
 require("dotenv").config();
 
-if(!process.env.REDIS_URL)
-   console.log('URL Redis Inválida')
-else
-   console.log('URL Redis válida')
+
 
 const nossoBot = new Twit({
    consumer_key: process.env.CONSUMER_KEY,
@@ -18,62 +15,18 @@ const nossoBot = new Twit({
    timeout_ms: 60 * 1000
 });
 
-const options = {
-   url: process.env.REDIS_URL,
-   enable_offline_queue: true,
-   no_ready_check: true,
-   retry_strategy: (options) => {
-     if (options.error && options.error.code === 'ECONNREFUSED') {
-       return new Error('The server refused the connection');
-     }
-     if (options.total_retry_time > 1000 * 60 * 60) {
-       return new Error('Retry time exhausted');
-     }
-     if (options.attempt > 10) {
-       return undefined;
-     }
-     // reconnect after
-     return Math.min(options.attempt * 100, 3000);
-   }
- };
-
-//const client = redis.createClient({url: process.env.REDIS_URL});
 
 async function acaoDoNossoBot() {
    console.log('zzzzz');
-  
-   const client = redis.createClient(
-   process.env.REDIS_URL,   
-   options
-   );
-  
 
-  let a
-   
-   await client.get('jsondata', (err, reply) => {
-      if (err) throw err;
-      console.log('AAAAAAAAHHHHHHHHHHHH',reply);
-  });
 
-/*
-(async () => {
-   const client = redis.createClient(process.env.REDIS_URL, options);
-   client.auth(process.env.REDIS_PW);
-   client.on('error', (err) => console.log('Redis Client Error', err));
- 
-   await client.connect();
- 
-   await client.set('key', 'PAPAPAPAAP');
-   const value = await client.get('key');
- })();
-*/
 // Cuidado ao postar tweets repetidos
 // increase 📈
 // decrease 📉
 
    let taxas = await taxa();
 
-   var postTweet = "📉 PREFIX-2025: "+ taxas.ipca2025Pre+"\n"+
+   var postTweet = "📉 xPREFIX-2025: "+ taxas.ipca2025Pre+"\n"+
                    "📉 PREFIX-2029: "+ taxas.ipca2029Pre+"\n"+
                    "📉 IPCA+2026: "+ taxas.ipca2026+"\n"+
                    "📉 IPCA+2035: "+ taxas.ipca2035+"\n"+
