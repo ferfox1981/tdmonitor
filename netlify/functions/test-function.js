58b49afbb09810270a80ecfd812476556e1c0e24
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2022-09-07 22:41:32 -0300

    ZZZZ

--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -1,11 +1,11 @@
 const { schedule } = require('@netlify/functions')
 
 const handler = async function(event, context) {
-    console.log("FODA-SE DEMAIS"+ new Date())
+    console.log("FODA-SE ZZZZZ"+ new Date())
 
     return {
         statusCode: 200,
     };
 };
 
-module.exports.handler = schedule("@hourly", handler);
+module.exports.handler = schedule("@hourly", handler); 
