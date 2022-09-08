7032764de18363782ac03a552068598048f06a23
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2022-09-08 10:55:41 -0300

    teste

--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -1,11 +1,11 @@
 const { schedule } = require('@netlify/functions')
 
 const handler = async function(event, context) {
-    console.log("De hora em hora GMT entre 9 e 17..."+ new Date())
+    console.log("De hora em hora GMT entre 9 e 17 APENAS UMA VEZ ..."+ new Date())
 
     return {
         statusCode: 200,
     };
 };
 
-module.exports.handler = schedule("6-14 * * * 1-5", handler); 
+module.exports.handler = schedule("1 9-14 * * 1-5", handler); 
