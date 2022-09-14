7f94ba2f85b17e62253c476de3cf21688a351bc8
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2022-09-13 22:51:06 -0300

    teste

--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -1,7 +1,25 @@
 const { schedule } = require('@netlify/functions')
+const firebase = require('../../firebase')
+
+var Twit = require("twit");
+
+const nossoBot = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000
+ });
+
+
 
 const handler = async function(event, context) {
     console.log("De hora em hora GMT entre 9:00 e 18:00 APENAS UMA VEZ ..."+ new Date())
+
+    let dadosAntigos = await firebase.getElement();
+   console.log('dadosAntigosw',dadosAntigos)
+
+
 
     return {
         statusCode: 200,
