63446e47ceb982ad64da7d8f6c9fa73707a48986
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2022-10-18 21:57:08 -0300

    agora vai

---
+++ netlify/functions/firebase.js
@@ -0,0 +1,31 @@
+const fs = require('firebase-admin');
+// https://retool.com/blog/crud-with-cloud-firestore-using-the-nodejs-sdk/
+// https://console.firebase.google.com/u/0/project/tdmonitor-1b233/settings/serviceaccounts/adminsdk?hl=pt
+// OK
+const serviceAccount = { 
+    "type": "service_account", 
+    "project_id": "tesouro-monitor",
+    "private_key_id": process.env.FIREBASE_PROJECT_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509
+};
+
+fs.initializeApp({
+ credential: fs.credential.cert(serviceAccount)
+});
+
+const db = fs.firestore();
+
+exports.getElement =  async function getElement() {
+    const dado = await db.collection('diario').doc('hora').get();
+    return dado.data();
+}
+
+exports.setElement = async function newSet(elem){
+    return await db.collection('diario').doc('hora').set(elem);
+}
--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -1,5 +1,5 @@
 const { schedule } = require('@netlify/functions')
-//const firebase = require('../../firebase')
+const firebase = require('./firebase')
 
 var Twit = require("twit");
  
@@ -14,10 +14,10 @@ const nossoBot = new Twit({
 
 
 const handler = async function(event, context) {
-    console.log("De hora em hora GMT entre 9:00 e 18:00 APENAS UMA VEZ ..."+ new Date())
+    console.log("ZZDe hora em hora GMT entre 9:00 e 18:00 APENAS UMA VEZ ..."+ new Date())
     
-    //let dadosAntigos = await firebase.getElement();
-   //console.log('dadosAntigosw',dadosAntigos)
+    let dadosAntigos = await firebase.getElement();
+   console.log('dadosAntigosw',dadosAntigos)
 
 
 
