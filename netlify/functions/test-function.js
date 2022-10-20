1e59484f2cf346314aa7e65b0b9a1c15b385c8f4
Fernando Raposo da Camara Silva <fernando.camara@serpro.gov.br>
2022-10-20 12:31:34 -0300

    CORRECAO

--- netlify/functions/test-function.js
+++ netlify/functions/test-function.js
@@ -26,7 +26,7 @@ const handler = async function (event, context) {
 
     let isOk = await firebase.setElement(taxas);
 
-    var postTweet = "pREFIX-2025: " + taxas.ipca2025Pre + util.gerarDiferencaPercentual(dadosAntigos.ipca2025Pre, taxas.ipca2025Pre) + "\n" +
+    var postTweet = "yyyREFIX-2025: " + taxas.ipca2025Pre + util.gerarDiferencaPercentual(dadosAntigos.ipca2025Pre, taxas.ipca2025Pre) + "\n" +
         "PREFIX-2029: " + taxas.ipca2029Pre + util.gerarDiferencaPercentual(dadosAntigos.ipca2029Pre, taxas.ipca2029Pre) + "\n" +
         "IPCA+2026: " + taxas.ipca2026 + util.gerarDiferencaPercentual(dadosAntigos.ipca2026, taxas.ipca2026) + "\n" +
         "IPCA+2035: " + taxas.ipca2035 + util.gerarDiferencaPercentual(dadosAntigos.ipca2035, taxas.ipca2035) + "\n" +
@@ -34,7 +34,7 @@ const handler = async function (event, context) {
 
     console.log('tweet vai ser:', postTweet)
     if(taxas.statusMercado !== 'Fechado' && taxas.statusMercado !== 'Em manutenção') {              
-
+        console.log('eNTROU')
         nossoBot.post(
             'statuses/update',
             { status: postTweet },
@@ -55,5 +55,5 @@ const handler = async function (event, context) {
         statusCode: 200,
     };
 };
-//module.exports.handler = schedule("* * * * *", handler);
-module.exports.handler = schedule("0 12-21 * * 1-5", handler); 
+module.exports.handler = schedule("* * * * *", handler);
+//module.exports.handler = schedule("0 12-21 * * 1-5", handler); 
